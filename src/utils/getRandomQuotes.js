f3785a3e90f9372082a8c049e833b93084347dd6
Misère <34185823+dannyrbk@users.noreply.github.com>
2023-05-27 23:22:49 +0300

    Add files via upload

---
+++ src/utils/getRandomQuotes.js
@@ -0,0 +1,19 @@
+const quotesArray = [
+	"Read the instructions",
+	"Super duper advanced",
+	"Blocking ads is better than blocking me",
+	"Advanced but without the ads",
+	"Just read the instructions!",
+	"Download Brave to support us",
+	"Download Adguard to support us",
+	"Vanced is no longer available",
+	"Vanced doesn't include video downloading",
+];
+
+const getRandomQuotes = () => {
+	const randomNumber = Math.floor(Math.random() * quotesArray.length);
+
+	return quotesArray[randomNumber];
+};
+
+export default getRandomQuotes;
