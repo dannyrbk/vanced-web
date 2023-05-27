b8e1c6d34c2b3b6ec17319bb2f36daee77ed2cd9
Misère <34185823+dannyrbk@users.noreply.github.com>
2023-05-27 23:23:04 +0300

    Add files via upload

---
+++ src/views/Home.js
@@ -0,0 +1,21 @@
+import React, {lazy, Suspense} from "react";
+// import sections
+import Hero from "../components/sections/Hero";
+import FeaturesTiles from "../components/sections/FeaturesTiles";
+import FeaturesSplit from "../components/sections/FeaturesSplit";
+//const FeaturesSplit = lazy(() => import("../components/sections/FeaturesSplit"));
+// import Testimonial from "../components/sections/Testimonial";
+// import Cta from "../components/sections/Cta";
+const Home = () => {
+	return (
+		<Suspense fallback={<div>Loading..</div>}>
+			<Hero className="illustration-section-01" />
+			<FeaturesTiles />
+			{/* <Testimonial topDivider /> */}
+			{/* <Cta split /> */}
+		</Suspense>
+		
+	);
+};
+
+export default Home;
